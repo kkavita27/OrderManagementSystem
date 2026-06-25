f36441bcd508ee2e62a30c41aa338a905d4d968b
Kavita <kavitakulkarni123@gmail.com>
2026-06-25 20:03:43 +0530

    added transactional

--- src/main/java/com/kulkarniGroups/OrderManagementSystem/POJO/OrderItemPOJO.java
+++ src/main/java/com/kulkarniGroups/OrderManagementSystem/POJO/OrderItemPOJO.java
@@ -1,4 +1,46 @@
 package com.kulkarniGroups.OrderManagementSystem.POJO;
 
 public class OrderItemPOJO {
-}
+
+//1. Create OrderItemPOJO
+//2. Add List<OrderItemPOJO> to OrderPOJO
+//3. Update createOrder() conversion logic
+//4. Test save
+//5. Add RuntimeException
+//6. Verify rollback
+
+    private String productName;
+
+    private Integer quantity;
+
+    private Double price;
+
+    // getters/setters
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+
+    }
+
